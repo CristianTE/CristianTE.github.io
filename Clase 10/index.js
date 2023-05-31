c88d4d214c8de92e54612c25eeceff353cd7104f
CristianTE <dia.torrealbacristian@gmail.com>
2023-05-30 23:18:16 -0300

    first commit
    Clase 10

---
+++ Clase 10/index.js
@@ -0,0 +1,11 @@
+const nombre = "Cristian"; // String
+const apellido = "Torrealba"; // String
+console.log(nombre +""+ apellido);
+
+const a = 1000; // Number
+const b = 2000; // Number
+const suma = 3000; // Number
+console.log(a + b + suma);
+
+const c = (suma + 4000); // Number
+console.log(suma + c);
